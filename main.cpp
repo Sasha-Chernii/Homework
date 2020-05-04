699cab26d15f3d7cf7ce442dfdc9fa28d63a3963
Sasha-Chernii <61801229+Sasha-Chernii@users.noreply.github.com>
2020-05-04 17:47:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+	Виведіть на екран число 255 в різних системах числення (окрім 2-кової) використовуючи маніпулятори dec,hex,oct.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << dec << 255 << endl;
+	cout << hex << 255 << endl;
+	cout << oct << 255 << endl;
+
+	system("pause");
+	return 0;
+}
